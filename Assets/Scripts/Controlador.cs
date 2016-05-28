9629e8477165e3f3cf9afbbb4af963ce4e6d5e24
F3nix123 <ignaciomurillo97@gmail.com>
2016-05-28 12:47:30 -0600

    Actualizacion

--- Assets/Scripts/Controlador.cs
+++ Assets/Scripts/Controlador.cs
@@ -4,8 +4,7 @@ using System.Collections;
 public class Controlador : MonoBehaviour {
 
     [Header("Compuertas")]
-    public bool compuerta = false;
-    public string[] compuertasDisponibles = {"and", "or", "nand", "nor", "xnor"};
+    public bool compuertaEscojida = false;
     public string compuertaSeleccionada;
 
     public GameObject[] objetos;
@@ -25,7 +24,7 @@ public class Controlador : MonoBehaviour {
     void Update() {
         //colocar una compuerta;
 
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(2) && objetoSeleccionado != null)
         {
             Ray ray = cam.ScreenPointToRay(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z));
             RaycastHit hit = new RaycastHit();
@@ -35,9 +34,52 @@ public class Controlador : MonoBehaviour {
                 if (hit.transform.tag == "Floor")
                 {
                     GameObject obj = Instantiate(objetoSeleccionado, hit.point + Vector3.up * 0.36f, Quaternion.identity) as GameObject;
+                    if (compuertaEscojida)
+                    {
+                        obj.GetComponent<Compuerta>().tipo = compuertaSeleccionada;
+                    }                    
                 }
             }
         }
 
 	}
+
+    void EscojerInput()
+    {
+        objetoSeleccionado = objetos[1];
+    }
+
+    void EscojerOutput()
+    {
+        objetoSeleccionado = objetos[2];
+        compuertaEscojida = false;
+    }
+
+    void EscojerAND ()
+    {
+        objetoSeleccionado = objetos[0];
+        compuertaSeleccionada = "and";
+        compuertaEscojida = false;
+    }
+
+    void EscojerOR()
+    {
+        objetoSeleccionado = objetos[0];
+        compuertaSeleccionada = "or";
+        compuertaEscojida = true;
+    }
+
+    void EscojerNAND()
+    {
+        objetoSeleccionado = objetos[0];
+        compuertaSeleccionada = "nand";
+        compuertaEscojida = true;
+    }
+
+    void EscojerNOR()
+    {
+        objetoSeleccionado = objetos[0];
+        compuertaSeleccionada = "nor";
+        compuertaEscojida = true;
+    }
 }
